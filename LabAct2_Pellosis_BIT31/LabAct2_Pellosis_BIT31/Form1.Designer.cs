8fa0a9a3624a3b0fb7c1497fe89a1e99499cc4ab
unknown <carljerichopel@gmail.com>
2024-09-10 21:57:43 +0800

    initial commit

---
+++ LabAct2_Pellosis_BIT31/LabAct2_Pellosis_BIT31/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace LabAct2_Pellosis_BIT31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblnum2 = new System.Windows.Forms.Label();
+            this.lbloper = new System.Windows.Forms.Label();
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Controls.Add(this.lblnum2);
+            this.groupBox1.Controls.Add(this.lbloper);
+            this.groupBox1.Controls.Add(this.lblnum1);
+            this.groupBox1.Controls.Add(this.txtOutput);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(559, 241);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Question";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(148, 193);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 33);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(148, 154);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 33);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(6, 154);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(136, 33);
+            this.txtInput.TabIndex = 4;
+            // 
+            // lblnum2
+            // 
+            this.lblnum2.AutoSize = true;
+            this.lblnum2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnum2.Location = new System.Drawing.Point(6, 97);
+            this.lblnum2.Name = "lblnum2";
+            this.lblnum2.Size = new System.Drawing.Size(51, 21);
+            this.lblnum2.TabIndex = 2;
+            this.lblnum2.Text = "label1";
+            // 
+            // lbloper
+            // 
+            this.lbloper.AutoSize = true;
+            this.lbloper.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbloper.Location = new System.Drawing.Point(6, 65);
+            this.lbloper.Name = "lbloper";
+            this.lbloper.Size = new System.Drawing.Size(51, 21);
+            this.lbloper.TabIndex = 1;
+            this.lbloper.Text = "label1";
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnum1.Location = new System.Drawing.Point(6, 30);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(51, 21);
+            this.lblnum1.TabIndex = 1;
+            this.lblnum1.Text = "label1";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(250, 19);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(303, 216);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Text = "";
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(8, 9);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(52, 21);
+            this.lblTimer.TabIndex = 5;
+            this.lblTimer.Text = "label4";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 6000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 286);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label lbloper;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblnum2;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ LabAct2_Pellosis_BIT31/LabAct2_Pellosis_BIT31/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabAct2_Pellosis_BIT31
+{
+    public partial class Form1 : Form
+    {
+        private Timer quizTime;
+        private int remTime = 60;
+        private double ans;
+        private string randOper;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (remTime > 0)
+            {
+                remTime--;
+                lblTimer.Text = "You have " + remTime.ToString() + " Seconds Left!";
+            }
+            else
+            {
+                quizTime.Stop();
+                lblTimer.Text = "Times Up!";
+            }
+        }
+        private void initialize()
+        {
+            generate();
+            startTime();
+        }
+
+        private void startTime()
+        {
+            quizTime = new Timer();
+            quizTime.Interval = 1000;
+            quizTime.Tick += timer1_Tick;
+            quizTime.Start();
+        }
+
+        private void generate()
+        {
+            Random rand = new Random();
+
+            string[] operations = { "+", "-", "*", "/" };
+            randOper = operations[rand.Next(operations.Length)];
+
+            int num1 = rand.Next(1, 99);
+            lbloper.Text = randOper.ToString();
+            int num2 = rand.Next(1, 99);
+
+            lblnum1.Text = num1.ToString();
+            lblnum2.Text = num2.ToString();
+
+            switch (randOper)
+            {
+                case "+":
+                    ans = num1 + num2;
+                    break;
+                case "-":
+                    ans = num1 - num2;
+                    break;
+                case "*":
+                    ans = num1 * num2;
+                    break;
+                case "/":
+                    ans = (double)num1 / num2;
+                    break;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generate();
+            startTime();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double userInput;   
+
+            if (double.TryParse(txtInput.Text, out userInput))
+            {
+                txtOutput.AppendText(lblnum1.Text.ToString() + " " + lbloper.Text.ToString() + " " + lblnum2.Text.ToString() + " Your answer is: " + txtInput.Text);
+                if (Math.Abs(userInput - ans) < 0.01)
+                {
+                    txtOutput.AppendText("✔️\n");
+                }
+                else
+                {
+                    txtOutput.AppendText("❌\n");
+                }
+            }
+
+            btnSubmit.Enabled = false; 
+            
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            btnSubmit.Enabled = true;
+            generate();
+            remTime = 60;
+        }
+    }
+}
