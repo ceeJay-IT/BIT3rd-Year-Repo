9cd29211a74e5fc6295c25ff9ef3fdbf2dc00290
unknown <carljerichopel@gmail.com>
2024-09-17 20:35:32 +0800

    initial commit

---
+++ LabAct3_Phonebook_Pellosis_BIT31/LabAct3_Phonebook_Pellosis_BIT31/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace LabAct3_Phonebook_Pellosis_BIT31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.mtxtphNum = new System.Windows.Forms.MaskedTextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.phNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.mtxtphNum);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 157);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Phonebook";
+            // 
+            // mtxtphNum
+            // 
+            this.mtxtphNum.Location = new System.Drawing.Point(108, 57);
+            this.mtxtphNum.Mask = "(999) 000-0000";
+            this.mtxtphNum.Name = "mtxtphNum";
+            this.mtxtphNum.Size = new System.Drawing.Size(176, 27);
+            this.mtxtphNum.TabIndex = 9;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(189, 122);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 26);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(189, 90);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 26);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(108, 122);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 26);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(108, 90);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 26);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(108, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(176, 27);
+            this.txtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(59, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.phNum,
+            this.phName});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 208);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(293, 318);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // phNum
+            // 
+            this.phNum.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.phNum.HeaderText = "Phone Number";
+            this.phNum.Name = "phNum";
+            // 
+            // phName
+            // 
+            this.phName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.phName.HeaderText = "Name";
+            this.phName.Name = "phName";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Contact Details";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(121, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "View";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 536);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mtxtphNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phName;
+    }
+}
+
---
+++ LabAct3_Phonebook_Pellosis_BIT31/LabAct3_Phonebook_Pellosis_BIT31/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dataHelper;
+using static dataHelper.DataHelper;
+
+namespace LabAct3_Phonebook_Pellosis_BIT31
+{
+    public partial class Form1 : Form
+    {
+        private Phonebook phonebook = new Phonebook();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = mtxtphNum.Text;
+            string name = txtName.Text;
+
+            if (phonebook.AddContact(phoneNumber, name))
+            {
+                RefreshDataGridView();
+                MessageBox.Show("Contact added!");
+            }
+            else
+            {
+                MessageBox.Show("A contact of with the same number has been inputted.");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = mtxtphNum.Text.Trim();
+            string name = txtName.Text.Trim();
+
+            dataGridView1.Rows.Clear();
+
+            Contact contact = null;
+
+            if (!string.IsNullOrEmpty(phoneNumber) && !string.IsNullOrEmpty(name))
+            {
+                contact = phonebook.searchPhoneNum(phoneNumber);
+                if (contact == null)
+                {
+                    contact = phonebook.searchName(name);
+                }
+            }
+            else if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                contact = phonebook.searchPhoneNum(phoneNumber);
+            }
+            else if (!string.IsNullOrEmpty(name))
+            {
+                contact = phonebook.searchName(name);
+            }
+            else
+            {
+                MessageBox.Show("Enter a Phone Number or Name to search!");
+                return;
+            }
+
+            if (contact != null)
+            {
+                dataGridView1.Rows.Add(contact.PhoneNumber, contact.Name);
+            }
+            else
+            {
+                MessageBox.Show("Contact Not Found!");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = mtxtphNum.Text;
+            string newName = txtName.Text;
+
+            if (phonebook.EditContact(phoneNumber, newName))
+            {
+                RefreshDataGridView();
+                MessageBox.Show("Contact Updated!");
+            }
+            else
+            {
+                MessageBox.Show("Contact not Found!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string phoneNumber = mtxtphNum.Text;
+
+            if (phonebook.DeleteContact(phoneNumber))
+            {
+                RefreshDataGridView();
+                MessageBox.Show("Contact Deleted!");
+            }
+            else
+            {
+                MessageBox.Show("Contact Not Found!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefreshDataGridView();
+        }
+
+        private void RefreshDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (Contact contact in phonebook.GetAllContacts())
+            {
+                dataGridView1.Rows.Add(contact.PhoneNumber, contact.Name);
+            }
+        }
+
+        private bool IsPhoneNumber(string text)
+        {
+            string cleanedText = new string(text.Where(char.IsDigit).ToArray());
+            return cleanedText.Length >= 10;
+        }
+    }
+}
